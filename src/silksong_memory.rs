c682d6d7cbd63ba9d6462a661fc74beba4df3a2d
PEOJI <147530430+PEOJI@users.noreply.github.com>
2025-10-22 20:24:57 +0100

    Trobbio Encountered split added

--- src/silksong_memory.rs
+++ src/silksong_memory.rs
@@ -275,6 +275,7 @@ declare_pointers!(PlayerDataPointers {
     encountered_song_chevalier_boss: UnityPointer<3> = pdp("encounteredSongChevalierBoss"),
     defeated_song_chevalier_boss: UnityPointer<3> = pdp("defeatedSongChevalierBoss"),
     completed_library_entry_battle: UnityPointer<3> = pdp("completedLibraryEntryBattle"),
+    encountered_trobbio: UnityPointer<3> = pdp("encounteredTrobbio"),
     defeated_trobbio: UnityPointer<3> = pdp("defeatedTrobbio"),
     has_harpoon_dash: UnityPointer<3> = pdp("hasHarpoonDash"),
     hang04_battle: UnityPointer<3> = pdp("hang04Battle"),
--- src/splits.rs
+++ src/splits.rs
@@ -501,6 +501,10 @@ pub enum Split {
     ///
     /// Splits when entering Songclave
     EnterSongclave,
+    /// Trobbio Encountered (Boss)
+    /// 
+    /// Splits when first encountering the Trobbio boss
+    TrobbioEncountered,
     /// Trobbio (Boss)
     ///
     /// Splits when killing Trobbio
@@ -2037,6 +2041,7 @@ pub fn continuous_splits(
         Split::GivenCouriersRasher => {
             should_split(mem.deref(&pd.gourmand_given_meat).unwrap_or_default())
         }
+        Split::TrobbioEncountered => should_split(mem.deref(&pd.encountered_trobbio).unwrap_or_default()),
         Split::Trobbio => should_split(mem.deref(&pd.defeated_trobbio).unwrap_or_default()),
         //endregion: ChoralChambers
 
